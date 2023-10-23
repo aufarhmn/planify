d9eadbd11cba5452d780918e9aa00ceb1dfeef8b
NandoAACF <nando.aacf88@gmail.com>
2023-10-23 09:13:05 +0700

    fix: fixing folder path

--- Planify/Planify/Login.cs
+++ Planify/Planify/Login.cs
@@ -8,16 +8,33 @@ namespace Planify
 {
     internal class Login
     {
+        private string _userId;
+        private string _name;
         private string _email;
         private string _password;
+        private string _fotoProfil;
 
-        public string Email
+        public string userId
+        {
+            get { return _userId; }
+        }
+        public string name
+        {
+            get { return _name; }
+        }
+
+        public string email
         {
             get { return _email; }
         }
-        public string Password
+
+        public string password
         {
             get { return _password; }
+        }
+        public string fotoProfil
+        {
+            get { return _fotoProfil; }
         }
     }
 }
